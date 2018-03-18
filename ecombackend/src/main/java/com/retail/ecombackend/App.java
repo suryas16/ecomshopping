69fdbd167882f49abd47139dbc016ae31bb4514f
ecom shopper <sy4usa@gmail.com>
2018-03-18 15:03:35 -0400

    My ecommerce project setup

---
+++ ecombackend/src/main/java/com/retail/ecombackend/App.java
@@ -0,0 +1,13 @@
+package com.retail.ecombackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
